15f04e88a180b7da7f012f86eb61cd0d7a61d79c
Yungchih Chen <yungchih_chen@yahoo.com>
2012-10-25 22:23:29 -0400

    Updated layout and added home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Jobs::Application.routes.draw do
+  get "home/index"
+
   resources :posts
 
   devise_for :users
@@ -52,7 +54,7 @@ Jobs::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'posts#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
