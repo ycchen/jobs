ca604a307e23faf2a68707e112f1b82f0affbc0a
Yungchih Chen <yungchih_chen@yahoo.com>
2012-10-31 21:48:15 -0400

    added relationship between user and job models, addedauthenticate_user before create, update or delete jobs, also identified devise helpers, as well as enable yaml format on the rails console.

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,10 +1,21 @@
 class Job < ActiveRecord::Base
   attr_accessible :title, :job_type, :occupation, :company_name, :location, :url, :description, :apply_information, :deadline, :user_id, :deadline_forever
 
-
   JOB_TYPE = %w[Full-time Part-time Contract Internship Other]
   OCCUPATION = 	['Web back-end', 'Web front-end', 'Web-design', 'QA/Testing', 'Other']
 
+  validates_presence_of :title
+  validates_presence_of :job_type
+  validates_presence_of :company_name
+  validates_presence_of :occupation
+  validates_presence_of :location
+  validates_presence_of :description
+  validates_presence_of :apply_information
+
+  validates_inclusion_of :job_type, :in => JOB_TYPE
+  validates_inclusion_of :occupation, :in => OCCUPATION
+
+  
   def deadline_forever
   	@deadline_forever ||= !self.deadline
   end
